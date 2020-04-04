43368f47121d34abc145da3d0106c56e06cd5765
Kilian Drechsler <kilian.drechsler@posteo.de>
2020-04-04 15:22:32 +0200

    added orginal

---
+++ orginal.js
@@ -0,0 +1,67 @@
+class windowManager {
+  constructor() {
+    browser.contextMenus.onClicked.addListener(() => {
+      this.merge();
+    });
+    browser.windows.onFocusChanged.addListener(() => {
+      this.calculateContextMenu();
+    });
+    this.calculateContextMenu();
+  }
+
+  async getCurrentWindows() {
+    const currentWindow = await browser.windows.getCurrent();
+    const windows = await browser.windows.getAll({});
+    return windows.filter((windowObj) => {
+      return windowObj.incognito === currentWindow.incognito;
+    });
+  }
+
+  async calculateContextMenu() {
+    const windows = await this.getCurrentWindows();
+    const id = "merge-windows";
+    browser.contextMenus.remove(id);
+    if (windows.length > 1) {
+      browser.contextMenus.create({
+        id,
+        title: "Merge all windows",
+        contexts: ["all", "tab"]
+      });
+    }
+  }
+
+  async merge() {
+    const windowMap = new Map();
+    const windows = await this.getCurrentWindows();
+    let biggestCount = 0;
+    let biggest = null;
+    let repin = [];
+    const promises = windows.map(async function (windowObj) {
+      const tabs = await browser.tabs.query({windowId: windowObj.id});
+      windowMap.set(windowObj, tabs.map((tab) => {
+        if (tab.pinned) {
+          repin.push(browser.tabs.update(tab.id, {pinned: false}));
+        }
+        return tab.id;
+      }));
+      if (tabs.length > biggestCount) {
+        biggest = windowObj;
+        biggestCount = tabs.length;
+      }
+    });
+    await Promise.all(promises);
+    const repinTabs = await Promise.all(repin);
+    windows.forEach((windowObj) => {
+      if (windowObj === biggest) {
+        return;
+      }
+      browser.tabs.move(windowMap.get(windowObj), {index: -1, windowId: biggest.id});
+    });
+    repinTabs.forEach((tab) => {
+      browser.tabs.update(tab.id, {pinned: true});
+    });
+    this.calculateContextMenu();
+  }
+};
+
+new windowManager();
