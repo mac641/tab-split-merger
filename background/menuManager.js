481f17bcffcc0bd220caff79d6ff56964ccc2d62
Marcel Arndt <master.tww@web.de>
2020-09-26 18:15:16 +0200

    Reformat js files

--- background/menuManager.js
+++ background/menuManager.js
@@ -1,135 +1,137 @@
 export class menuManager {
   constructor() {
     // Class attributes
-    this.managerId = "tabSplitMerger";
-    this.thresholdId = "configConfirmationThreshold";
-    this.splitAllId = "splitAllTabs";
-    this.splitCurrentId = "splitCurrentTabs";
-    this.mergeId = "mergeWindows";
-    this.repinId = "configRepinTabs";
+    this.managerId = 'tabSplitMerger'
+    this.thresholdId = 'configConfirmationThreshold'
+    this.splitAllId = 'splitAllTabs'
+    this.splitCurrentId = 'splitCurrentTabs'
+    this.splitLeftId = 'splitLeft'
+    this.splitRightId = 'splitRight'
+    this.mergeId = 'mergeWindows'
+    this.repinId = 'configRepinTabs'
 
     // call startup functions
-    this.init();
-    this.calculateTemporaryMenu();
+    this.init()
+    this.calculateTemporaryMenu()
 
     // add event listeners
     browser.windows.onFocusChanged.addListener(() => {
-      this.calculateTemporaryMenu();
-    });
+      this.calculateTemporaryMenu()
+    })
     browser.tabs.onCreated.addListener(() => {
-      this.calculateTemporaryMenu();
-    });
+      this.calculateTemporaryMenu()
+    })
     browser.tabs.onAttached.addListener(() => {
-      this.calculateTemporaryMenu();
-    });
+      this.calculateTemporaryMenu()
+    })
     browser.tabs.onRemoved.addListener(() => {
-      this.calculateTemporaryMenu();
-    });
+      this.calculateTemporaryMenu()
+    })
   }
 
   async init() {
     // Add constant menu items
-    browser.menus.remove(this.managerId);
+    browser.menus.remove(this.managerId)
     browser.menus.create({
       id: this.managerId,
-      title: "Tab Split Merger",
-      contexts: ["all", "tab"]
-    });
-    browser.menus.remove(this.thresholdId);
+      title: 'Tab Split Merger',
+      contexts: ['all', 'tab']
+    })
+    browser.menus.remove(this.thresholdId)
     browser.menus.create({
       id: this.thresholdId,
-      title: "Set confirmation threshold",
-      contexts: ["all", "tab"],
+      title: 'Set confirmation threshold',
+      contexts: ['all', 'tab'],
       parentId: this.managerId
-    });
-    browser.menus.remove(this.repinId);
+    })
+    browser.menus.remove(this.repinId)
     browser.menus.create({
       id: this.repinId,
-      type: "checkbox",
-      title: "Repin tabs",
-      contexts: ["all", "tab"],
+      type: 'checkbox',
+      title: 'Repin tabs',
+      contexts: ['all', 'tab'],
       checked: true,
       parentId: this.managerId
-    });
-    browser.menus.remove("separator-1");
+    })
+    browser.menus.remove('separator-1')
     browser.menus.create({
-      id: "separator-1",
-      type: "separator",
-      contexts: ["all", "tab"],
+      id: 'separator-1',
+      type: 'separator',
+      contexts: ['all', 'tab'],
       parentId: this.managerId
-    });
+    })
   }
 
   async getCurrentWindows() {
     // get current window as window.Window object and all windows as window.Window array
-    const currentWindow = await browser.windows.getCurrent();
-    const windows = await browser.windows.getAll({});
+    const currentWindow = await browser.windows.getCurrent()
+    const windows = await browser.windows.getAll({})
 
     // Remove incognito windows and return
     return windows.filter((windowObj) => {
-      return windowObj.incognito === currentWindow.incognito;
-    });
+      return windowObj.incognito === currentWindow.incognito
+    })
   }
 
   async calculateTemporaryMenu() {
     await this.getCurrentWindows().then(
       (value) => (this.windows = value),
       (error) => console.error(error)
-    );
-    this.calculateSplitTabsMenu();
-    this.calculateMergeMenu();
+    )
+    this.calculateSplitTabsMenu()
+    this.calculateMergeMenu()
   }
 
   async calculateSplitTabsMenu() {
-    browser.menus.remove(this.splitAllId);
-    browser.menus.remove(this.splitCurrentId);
+    browser.menus.remove(this.splitAllId)
+    browser.menus.remove(this.splitCurrentId)
 
     // check how many tabs exist and create split menu if there are at least 2
     browser.tabs.query({}).then(
       (tabs) => {
         this.windows.map((window) => {
-          let tabByWindowCount = 0;
+          let tabByWindowCount = 0
           tabs.forEach((tab) => {
-            if (tab.windowId === window.id) tabByWindowCount += 1;
-          });
+            if (tab.windowId === window.id) tabByWindowCount += 1
+          })
 
           if (tabByWindowCount > 1) {
             if (window.focused === true) {
-              browser.menus.remove(this.splitCurrentId);
+              browser.menus.remove(this.splitCurrentId)
               browser.menus.create({
                 id: this.splitCurrentId,
-                title: "Split tabs in current window",
-                contexts: ["all", "tab"],
+                title: 'Split tabs in current window',
+                contexts: ['all', 'tab'],
                 parentId: this.managerId
-              });
+              })
             }
             if (this.windows.length > 1) {
-              browser.menus.remove(this.splitAllId);
+              browser.menus.remove(this.splitAllId)
               browser.menus.create({
                 id: this.splitAllId,
-                title: "Split tabs in all windows",
-                contexts: ["all", "tab"],
+                title: 'Split tabs in all windows',
+                contexts: ['all', 'tab'],
                 parentId: this.managerId
-              });
+              })
             }
           }
-        });
+        })
       },
       (error) => {
-        return console.error(error);
+        return console.error(error)
       }
-    );
+    )
   }
 
   async calculateMergeMenu() {
-    browser.menus.remove(this.mergeId);
+    browser.menus.remove(this.mergeId)
     if (this.windows.length > 1) {
       browser.menus.create({
         id: this.mergeId,
-        title: "Merge windows",
-        contexts: ["all", "tab"],
+        title: 'Merge windows',
+        contexts: ['all', 'tab'],
         parentId: this.managerId
-      });
+      })
     }
   }
 }
--- background/windowManager.js
+++ background/windowManager.js
@@ -1,221 +1,221 @@
-import { configManager } from "./configManager.js";
-import { menuManager } from "./menuManager.js";
+import { configManager } from './configManager.js'
+import { menuManager } from './menuManager.js'
 
 class windowManager {
   constructor() {
     // Class attributes
-    this.config = new configManager();
-    this.menu = new menuManager();
+    this.config = new configManager()
+    this.menu = new menuManager()
 
     // call startup functions
-    this.init();
+    this.init()
   }
 
   async init() {
     browser.menus.onClicked.addListener(async (info) => {
       if (info.menuItemId === this.menu.splitAllId) {
-        this.split(true);
+        this.split(true)
       } else if (info.menuItemId === this.menu.splitCurrentId) {
-        this.split(false);
+        this.split(false)
       } else if (info.menuItemId === this.menu.thresholdId) {
-        this.config.askForCurrentConfig();
+        this.config.askForCurrentConfig()
       } else if (info.menuItemId === this.menu.mergeId) {
-        this.merge();
+        this.merge()
       } else if (info.menuItemId === this.menu.repinId) {
         let config = await this.config.getConfiguration().then(
           (value) => value,
           (error) => console.error(error)
-        );
+        )
         if (info.checked) {
-          config.repinTabs = true;
+          config.repinTabs = true
         } else {
-          config.repinTabs = false;
+          config.repinTabs = false
         }
-        this.config.setConfiguration(config);
+        this.config.setConfiguration(config)
       }
-    });
+    })
 
     browser.commands.onCommand.addListener(async (command) => {
       if (command === this.menu.splitAllId) {
-        this.split(true);
+        this.split(true)
       } else if (command === this.menu.splitCurrentId) {
-        this.split(false);
+        this.split(false)
       } else if (command === this.menu.mergeId) {
-        this.merge();
+        this.merge()
       }
-    });
+    })
   }
 
   async getCurrentWindows() {
     // get current window as window.Window object and all windows as window.Window array
-    const currentWindow = await browser.windows.getCurrent();
-    const windows = await browser.windows.getAll({});
+    const currentWindow = await browser.windows.getCurrent()
+    const windows = await browser.windows.getAll({})
 
     // Remove incognito windows and return
     return windows.filter((windowObj) => {
-      return windowObj.incognito === currentWindow.incognito;
-    });
+      return windowObj.incognito === currentWindow.incognito
+    })
   }
 
   async split(all) {
     // ask for confirmation before splitting tabs into windows
-    let isSplit = false;
+    let isSplit = false
     await this.config.askForSplitPermission(all).then(
       (value) => (isSplit = value[0]),
       (error) => console.error(error)
-    );
+    )
 
     if (!isSplit) {
-      return;
+      return
     }
 
-    let windows = [];
+    let windows = []
     if (all) {
       windows = await this.getCurrentWindows().then(
         (value) => value,
         (error) => console.error(error)
-      );
+      )
     } else {
-      windows.push(await browser.windows.getCurrent());
+      windows.push(await browser.windows.getCurrent())
     }
 
     const config = await this.config.getConfiguration().then(
       (value) => value,
       (error) => console.error(error)
-    );
+    )
 
     // For each window get all tabs, push pinned tabs into repin array after unpinning
     // and push newly created windows into createdWindowsPromises array
-    let repin = [];
-    let lastActiveTab = null;
-    let createdWindowsPromises = [];
+    let repin = []
+    let lastActiveTab = null
+    let createdWindowsPromises = []
     const promises = windows.map(async (windowObj) => {
       const tabs = await browser.tabs.query({
         windowId: windowObj.id
-      });
+      })
       tabs.map((tab) => {
         if (tab.pinned) {
           repin.push(
             browser.tabs.update(tab.id, {
               pinned: false
             })
-          );
+          )
         }
         if (windowObj.focused && tab.active) {
-          lastActiveTab = tab;
+          lastActiveTab = tab
         }
-        return tab.id;
-      });
+        return tab.id
+      })
       if (tabs.length < 2) {
-        return;
+        return
       }
       tabs.map(async (tab) => {
         createdWindowsPromises.push(
           browser.windows.create({
             tabId: tab.id
           })
-        );
-      });
-    });
+        )
+      })
+    })
 
     // Solve all Promises and repin previously unpinned tabs
-    await Promise.all(promises);
-    const repinTabs = await Promise.all(repin);
-    const createdWindows = await Promise.all(createdWindowsPromises);
+    await Promise.all(promises)
+    const repinTabs = await Promise.all(repin)
+    const createdWindows = await Promise.all(createdWindowsPromises)
 
     if (config.repinTabs) {
-      this.afterRepin(repinTabs);
+      this.afterRepin(repinTabs)
     }
-    this.refocus(lastActiveTab, createdWindows);
+    this.refocus(lastActiveTab, createdWindows)
 
-    this.menu.calculateTemporaryMenu();
+    this.menu.calculateTemporaryMenu()
   }
 
   async merge() {
     const windows = await this.getCurrentWindows().then(
       (value) => value,
       (error) => console.error(error)
-    );
+    )
 
     const config = await this.config.getConfiguration().then(
       (value) => value,
       (error) => console.error(error)
-    );
+    )
 
     // For each window map tabs to window objects, unpin pinned tabs and push them into repin array
-    const windowMap = new Map();
-    let biggestCount = 0;
-    let biggest = null;
-    let repin = [];
-    let lastActiveTab = null;
+    const windowMap = new Map()
+    let biggestCount = 0
+    let biggest = null
+    let repin = []
+    let lastActiveTab = null
     const promises = windows.map(async (windowObj) => {
       const tabs = await browser.tabs.query({
         windowId: windowObj.id
-      });
+      })
       windowMap.set(
         windowObj,
         tabs.map((tab) => {
           if (tab.pinned) {
-            repin.push(browser.tabs.update(tab.id, { pinned: false }));
+            repin.push(browser.tabs.update(tab.id, { pinned: false }))
           }
           if (windowObj.focused && tab.active) {
-            lastActiveTab = tab;
+            lastActiveTab = tab
           }
-          return tab.id;
+          return tab.id
         })
-      );
+      )
       if (tabs.length > biggestCount) {
-        biggest = windowObj;
-        biggestCount = tabs.length;
+        biggest = windowObj
+        biggestCount = tabs.length
       }
-    });
+    })
 
     // Solve all Promises and repin previously unpinned tabs
-    await Promise.all(promises);
-    const repinTabs = await Promise.all(repin);
+    await Promise.all(promises)
+    const repinTabs = await Promise.all(repin)
     windows.forEach(async (windowObj) => {
       if (windowObj === biggest) {
-        return;
+        return
       }
       browser.tabs.move(windowMap.get(windowObj), {
         index: -1,
         windowId: biggest.id
-      });
-    });
+      })
+    })
 
     if (config.repinTabs) {
-      this.afterRepin(repinTabs);
+      this.afterRepin(repinTabs)
     }
 
-    this.refocus(lastActiveTab, biggest);
+    this.refocus(lastActiveTab, biggest)
 
-    this.menu.calculateTemporaryMenu();
+    this.menu.calculateTemporaryMenu()
   }
 
   // Repin previously pinned tabs
   async afterRepin(repinTabs) {
     repinTabs.forEach(async (tab) => {
-      browser.tabs.update(tab.id, { pinned: true });
-    });
+      browser.tabs.update(tab.id, { pinned: true })
+    })
   }
 
   // Refocus previously selected tab
   async refocus(lastActiveTab, windows) {
     if (!Array.isArray(windows)) {
-      browser.tabs.update(lastActiveTab.id, { active: true });
-      return;
+      browser.tabs.update(lastActiveTab.id, { active: true })
+      return
     }
 
     windows.forEach((window) => {
-      const tabs = window.tabs;
+      const tabs = window.tabs
       tabs.forEach(async (tab) => {
         if (tab.id === lastActiveTab.id) {
-          browser.windows.update(window.id, { focused: true });
-          browser.tabs.update(lastActiveTab.id, { active: true });
+          browser.windows.update(window.id, { focused: true })
+          browser.tabs.update(lastActiveTab.id, { active: true })
         }
-      });
-    });
+      })
+    })
   }
 }
 
-new windowManager();
+new windowManager()
