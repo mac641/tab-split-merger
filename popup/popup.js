67a7c505d7b58ed150eb72e13401cf1505951b24
Marcel Arndt <master.tww@web.de>
2021-01-01 20:34:01 +0100

    Add first attempt for Firefox like styled toolbar menu

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,6 @@
-import { configManager } from "../background/configManager.js";
-import { menuManager } from "../background/menuManager.js";
-import { windowManager } from "../background/windowManager.js";
+import { configManager } from '../background/configManager.js'
+import { menuManager } from '../background/menuManager.js'
+import { windowManager } from '../background/windowManager.js'
 
 class popup {
   constructor() {}
